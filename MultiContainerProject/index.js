1a35ec765a61730adfa03a6eefd9dff59dfef76c
hisoka7i <rohit7i@outlook.com>
2024-08-10 22:35:39 +0530

    multi container docker file test

--- MultiContainerProject/index.js
+++ MultiContainerProject/index.js
@@ -1,11 +1,19 @@
-const expres = require('express');
+const express = require('express');
 const redis = require('redis');
 
 const app = express();
 
 // we need to specify host for the client
-const client = redis.createClient();
+//location of the redis server
+const client = redis.createClient({
+    //usually we put the entire address http:// .. 
+    //but since we are using docker, we do not have to specify the entire thing
+    host: 'redis-server',
+    port: 6379
+    //this will automatically redirect to the other container which is redis-server
+});
 //This will set initial visit a 0 
+
 client.set('visits', 0);
 
 app.get('/', (request,respose)=>{
@@ -13,10 +21,8 @@ app.get('/', (request,respose)=>{
         respose.send("Number of visits => " + visit);
         //we are showing the visits to application and then we are incrementing
         //the visit count into the database
-        client.set('visits', visit+1);
-    }).err(()=>{
-        console.log("We are getting error in this!!");
-    })
+        client.set('visits', (Number(visit)+1));
+    });
 });
 
 app.listen(8081, ()=>{
